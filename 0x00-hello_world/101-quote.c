9a91a05335c33f4218ad662e1a94a3a0ab85f88e
phetex <eassam89@gmail.com>
2022-09-09 13:34:00 -0700

    size

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - prints a text
+ *
+ * Return: always 1 (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
