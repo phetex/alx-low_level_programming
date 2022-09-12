d3fcebbadc20a33b186236f84b4228c86ea782b0
phetex <eassam89@gmail.com>
2022-09-12 07:06:29 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,19 @@ int maint(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+
 	{
 		printf("%d is positive\n", n);
 	}
+
 	else
+
 	{
 		printf("%d is zero\n", n);
 	}
+
 	else
+
 	{
 		printf("%d is negative\n");
 	}
