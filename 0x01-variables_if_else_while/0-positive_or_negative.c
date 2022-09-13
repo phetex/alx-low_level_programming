82edd3f2bb7e53916f73929167dd556bdd4059e1
phetex <eassam89@gmail.com>
2022-09-12 17:46:36 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
-* main - print if the unmber is positive, zero, or negative
-*
-* Return: Always (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 
-	{
-		printf("%d is positive\n");
-	}
+		printf("%d is positive\n", n);
 
-	else if (n < 0)
+	else if (n == 0)
 
-	{
-		printf("%d is negative\n");
-	}
-
+		printf("%d is zero\n", n);
 	else
+		printf("%d is negative\n", n);
 
-	{
-		printf("%d is zero\n");
-	}
-
-	return (0)
+	return (0);
 }
