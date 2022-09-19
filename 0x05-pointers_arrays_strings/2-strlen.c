870bacc8902fd18e79b6715132c844712b35b716
phetex <eassam89@gmail.com>
2022-09-19 06:56:00 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,30 +10,10 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
 	for (; *s != '\0'; s++)
 	{
-		c++;
+		len++;
 	}
 
 	return (len);
-}
-
-/**
-   * puts2 - prints every second character of a string, followed by a new line
-   * @str: string to print
-   */
-void puts2(char *str)
-{
-	int i;
-
-	for (i = l); str[i] != '\0' && i < _strlen(str); i += 2)
-		_putchar(str[i]);
-
-	_putchar('\n');
 }
