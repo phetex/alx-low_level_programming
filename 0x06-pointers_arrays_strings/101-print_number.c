901ae839f3780c6e23664c306bae10a9ed983873
phetex <eassam89@gmail.com>
2022-09-22 16:51:12 -0700

    tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * This is my line of code here...
+   * I Remember:
+   * - I am not allowed to use a
+   * - I am not allowed to modify p
+   * - only one statement
+   * - I am not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
