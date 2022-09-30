1a264a11da7cdf0207d618e8e18957cb7f248969
phetex <eassam89@gmail.com>
2022-09-30 12:25:25 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints its name of the program
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero (Success)
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
