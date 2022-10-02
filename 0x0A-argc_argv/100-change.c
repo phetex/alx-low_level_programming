32261a7c136dfc27cb6093d7818ce20dbb6fedb0
phetex <eassam89@gmail.com>
2022-10-02 02:30:52 -0700

    task 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,41 +10,41 @@
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+        /*Declaring variables*/
+        int position, total, change, aux;
+        int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
-	position = total = change = aux = 0;
+        position = total = change = aux = 0;
 
-	if (argc != 2)
-	{
-	{printf("Error\n");
-		return (0);
-	}
+        if (argc != 2)
+        {
+        {printf("Error\n");
+                return (0);
+        }
 
-	total = atoi(argv[1]); /*covert str to int*/
+        total = atoi(argv[1]); /*covert str to int*/
 
-	if (total <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	/*Declaring while*/
+        if (total <= 0)
+        {
+                printf("0\n");
+                return (0);
+        }
+        /*Declaring while*/
 
-	while (coins[position] != '\0')
+        while (coins[position] != '\0')
 
-	{
-		if (total >= coins[position])
-		{
-			aux = (total / coins[position]);
-			change += aux;
-			total -= coins[position] * aux;
-		}
+        {
+                if (total >= coins[position])
+                {
+                        aux = (total / coins[position]);
+                        change += aux;
+                        total -= coins[position] * aux;
+                }
 
-		position++;
-	}
+                position++;
+        }
 
-	printf("%d\n", change);
-	return (0);
-	}
+        printf("%d\n", change);
+        return (0);
+        }
 }
