976b2d0d119a5f4ffdb732e65ebc3a2100a849c9
phetex <eassam89@gmail.com>
2022-10-06 07:54:03 -0700

    tasks

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _calloc - allocated memory for nmemb element of size bytes
+  * @nmemb: number of element in the array
+  * @size: bytes for each position in array
+  *
+  * Return: pointer void
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
+}
