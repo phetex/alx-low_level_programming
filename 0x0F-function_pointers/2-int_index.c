55e1025ae88b4e863059a3931e768bdd12584632
phetex <eassam89@gmail.com>
2022-10-12 13:44:04 -0700

    tasks

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - Array index
+ * @array: Array
+ * @size: Size array
+ * @cmp: Function of pointer
+ * Return: First value
+ *
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
