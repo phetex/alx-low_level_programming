18c332bb094f327d6151352e09389ed2a507bae9
phetex <eassam89@gmail.com>
2022-10-29 07:34:54 -0700

    endiannee

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,17 +3,17 @@
 
 /**
 * get_endianness - determinate endianness of machine
-* Return: 1 if is little endian, 0 if is a big endian
+* Return: 1 if is little endian, 0 if is a big endian.
+*
 */
 
+int get_endianness(void)
+ {
+ unsigned int i = 1;
+ char *str = (char *) &i;
 
-int get_endianness(void)
-{
-	unsigned int i = 1;
-	char *c = (char *)&i;
-
-	if (*c)
-		return (1);
-	else
-		return (0);
-}
+ if (*str)
+ return (1);
+ else
+ return (0);
+ }
